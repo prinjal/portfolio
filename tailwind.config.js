f3da7e12fde3331abd856a3a2dd291dcb59f1d15
prinjal <prinjal.dave@gmail.com>
2024-04-07 02:13:37 -0700

    navbar implemented, opacity remaining

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         heroGradientStart: "rgb(110, 152, 248)",
         heroGradientVia: "rgb(236, 110, 86)",
         heroGradientEnd: "rgb(80, 130, 108)",
+        backgroundOpacity: ["active"],
         // You can add as many custom colors as you need
       },
     },
